d5327764337bc193fec03b321a6a2558c4486e2e
Roy1604 <23083000050@student.unmer.ac.id>
2024-05-13 19:33:52 +0700

    Add files via upload

---
+++ CalculatorToko.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class CalculatorToko {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        
+        System.out.print("Masukan harga Barang =");
+        double hargaBarang=scanner.nextDouble();
+        
+        System.out.print("masukan harga Sayuran =");
+        double hargaSayuran=scanner.nextDouble();
+        double subTotal=hargaBarang + hargaSayuran ;
+ 
+        double pajak =subTotal *0.1;
+        double tips =subTotal * 0.010;
+        double total=subTotal + pajak  + tips;
+        
+        System.out.println("subTotal :" + subTotal);
+        System.out.println("pajak :" + pajak);
+        System.out.println("tips :" + tips);
+        System.out.println("total :" + total);
+    }
+}
